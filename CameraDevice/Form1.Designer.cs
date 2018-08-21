740bf3304843dca8508ccda9010833cc241a7b2c
Edward <edward_song@yeah.net>
2018-08-21 11:13:00 +0800

    1st commit

---
+++ CameraDevice/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace CameraDevice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picDevice = new System.Windows.Forms.PictureBox();
+            this.combDeviceList = new System.Windows.Forms.ComboBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnSaveImg = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picDevice)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picDevice
+            // 
+            this.picDevice.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picDevice.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.picDevice.Location = new System.Drawing.Point(12, 26);
+            this.picDevice.Name = "picDevice";
+            this.picDevice.Size = new System.Drawing.Size(413, 320);
+            this.picDevice.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picDevice.TabIndex = 0;
+            this.picDevice.TabStop = false;
+            // 
+            // combDeviceList
+            // 
+            this.combDeviceList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.combDeviceList.FormattingEnabled = true;
+            this.combDeviceList.Location = new System.Drawing.Point(12, 368);
+            this.combDeviceList.Name = "combDeviceList";
+            this.combDeviceList.Size = new System.Drawing.Size(412, 20);
+            this.combDeviceList.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.AutoSize = true;
+            this.btnStart.Location = new System.Drawing.Point(11, 404);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(112, 44);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "&Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(166, 404);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 44);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "&Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnSaveImg
+            // 
+            this.btnSaveImg.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSaveImg.AutoSize = true;
+            this.btnSaveImg.Location = new System.Drawing.Point(312, 404);
+            this.btnSaveImg.Name = "btnSaveImg";
+            this.btnSaveImg.Size = new System.Drawing.Size(112, 44);
+            this.btnSaveImg.TabIndex = 4;
+            this.btnSaveImg.Text = "SavImg";
+            this.btnSaveImg.UseVisualStyleBackColor = true;
+            this.btnSaveImg.Click += new System.EventHandler(this.btnSaveImg_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 460);
+            this.Controls.Add(this.btnSaveImg);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.combDeviceList);
+            this.Controls.Add(this.picDevice);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picDevice)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picDevice;
+        private System.Windows.Forms.ComboBox combDeviceList;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnSaveImg;
+    }
+}
+
---
+++ CameraDevice/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using System.Drawing.Imaging;
+
+namespace CameraDevice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private FilterInfoCollection VideoCaptureDevices;
+        private VideoCaptureDevice FinalVideo;
+
+        int flag = 1;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            VideoCaptureDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo videoCaptureDevice in VideoCaptureDevices)
+            {
+                combDeviceList.Items.Add(videoCaptureDevice.Name);
+            }
+            combDeviceList.SelectedIndex = 0;
+            FinalVideo = new VideoCaptureDevice();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (FinalVideo.IsRunning)
+            {
+                FinalVideo.Stop();
+               // FinalVideo = null;
+                btnStart.Text = "&Start";
+                return;
+
+            }
+            FinalVideo = new VideoCaptureDevice(VideoCaptureDevices[combDeviceList.SelectedIndex].MonikerString);
+            FinalVideo.NewFrame += FinalVideo_NewFrame;
+            FinalVideo.Start();
+            btnStart.Text = "&Stop";
+        }
+
+        void FinalVideo_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            // throw new NotImplementedException();
+            Bitmap video = (Bitmap)eventArgs.Frame.Clone();
+            picDevice.Image = video;
+            //if (flag ==0)
+            //{
+            //    string file = Application.StartupPath + @"\" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".jpg";
+            //    video.Save(file);
+            //    flag = 1;
+            //    //FinalVideo.NewFrame += new NewFrameEventHandler(FinalVideo_NewFrame);
+
+            //    FinalVideo.SignalToStop();
+            //    FinalVideo.WaitForStop();
+            //    this.Close();
+
+            //}
+            
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (FinalVideo.IsRunning) FinalVideo.Stop();
+        }
+
+        private void btnSaveImg_Click(object sender, EventArgs e)
+        {
+            //flag = 0;
+            //FinalVideo.NewFrame += new NewFrameEventHandler(FinalVideo_NewFrame);
+
+
+            if (picDevice.Image != null)
+            {
+                Bitmap img = new Bitmap(picDevice.Image);
+                img.Save(Application.StartupPath + @"\" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".jpg", ImageFormat.Jpeg);
+                img.Dispose();
+                img = null;
+            }
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FinalVideo.SignalToStop();
+            FinalVideo.WaitForStop();
+            picDevice.Image = null;
+            picDevice.BackColor = Color.Black;
+        }
+    }
+}
